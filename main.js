d61a7fda7571ed48d71b9ac79ca43b4fd5373976
Shaun P Sullivan <shaun@fisheyesoup.com>
2015-07-27 13:55:29 -0400

    PARTIAL BREAKTHROUGH

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
 console.log ("hello from shaun sullivan");
+$(document).ready(function() {
+
+  function add(number1, number2) {
+    return Number(number1) + Number(number2);
+  };
+
+  function subtract(number1, number2) {
+    return Number(number1) - Number(number2);
+  };
+  function multiply(number1, number2) {
+    return Number(number1) * Number(number2);
+  };
+  function divide(number1, number2) {
+    return Number(number1) / Number(number2);
+  };
+
+  //events//
+
+  $('#addBtn').click(function(event){
+  event.preventDefault();
+  var num1 = $('#num_one').val();
+  var num2 = $('#num_two').val();
+  var total = add(num1, num2);
+  $('#answer').text(total);
+});
+
+$('#subBtn').click(function(event) {
+  event.preventDefault();
+  var num1 = $('#num_one').val();
+  var num2 = $('#num_two').val();
+  var total = subtract(num1, num2);
+  $('#answer').text(total);
+});
+
+$('#multiplyBtn').click(function(event) {
+  event.preventDefault();
+  var num1 = $('#num_one').val();
+  var num2 = $('#num_two').val();
+  var total = multiply(num1, num2);
+  $('#answer').text(total);
+});
+
+$('#divideBtn').click(function(event) {
+  event.preventDefault();
+  var num1 = $('#num_one').val();
+  var num2 = $('#num_two').val();
+  var total = divide(num1, num2);
+  $('#answer').text(total);
+});
+
+});
+
+
+
+
+
+
+}
+
+<div class = "result">
+Answer: <span id="result"></span>
+</div>
